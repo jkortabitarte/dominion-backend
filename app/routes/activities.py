f9d2f15c563b96155d177236f71ce1ca25a1f45f
jkortabitarte <jkortabitarte@gmail.com>
2026-01-24 10:44:58 +0100

    Add routes module

--- app/routes/activities.py
+++ app/routes/activities.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.models import Activity, TerritoryInfluence
+from app.utils.geo import polyline_to_h3
 import uuid
 
 router = APIRouter(prefix="/activities")
@@ -20,13 +21,30 @@ def fake_polyline_to_territory(polyline: str) -> str:
 @router.post("/")
 def create_activity(user_id: str, polyline: str, db: Session = Depends(get_db)):
     activity = Activity(
-        id=str(uuid.uuid4()),
         user_id=user_id,
         polyline=polyline,
     )
     db.add(activity)
 
-    territory_id = fake_polyline_to_territory(polyline)
+    hexes = polyline_to_h3(polyline)
+
+for hex_id in hexes:
+    influence = (
+        db.query(TerritoryInfluence)
+        .filter_by(territory_id=hex_id, user_id=user_id)
+        .first()
+    )
+
+    if influence:
+        influence.influence += 1
+    else:
+        influence = TerritoryInfluence(
+            territory_id=hex_id,
+            user_id=user_id,
+            influence=1,
+        )
+        db.add(influence)
+
 
     influence = (
         db.query(TerritoryInfluence)
