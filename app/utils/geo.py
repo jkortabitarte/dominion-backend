c15ced8eff12c39c558eee0486b574690b15cfb5
jkortabitarte <jkortabitarte@gmail.com>
2026-01-24 19:48:48 +0100

    Changes on Geo.py

--- app/utils/geo.py
+++ app/utils/geo.py
@@ -1,14 +1,19 @@
 import polyline
 import h3
 
-H3_RESOLUTION = 9  # buen equilibrio ciudad
+H3_RESOLUTION = 9  # buen equilibrio ciudad / barrio
+
 
 def polyline_to_h3(polyline_str: str) -> set[str]:
+    """
+    Convierte una polyline de Strava/Google en un set de hexágonos H3
+    """
     coords = polyline.decode(polyline_str)  # [(lat, lon), ...]
-    hexes = set()
+
+    hexes: set[str] = set()
 
     for lat, lon in coords:
-        hex_id = h3.geo_to_h3(lat, lon, H3_RESOLUTION)
+        hex_id = h3.latlng_to_cell(lat, lon, H3_RESOLUTION)
         hexes.add(hex_id)
 
     return hexes
